3976973d1b4864c95502e99c01edcee6be844cf6
neelmyna <nithincheeta@gmail.com>
2025-05-15 12:51:57 +0530

    mean median and std deviation

--- numpy/p15.py
+++ numpy/p15.py
@@ -8,5 +8,6 @@ a3 = np.array([0, 101, 323])
 # column_stack() 
 # The elements from the numpy arrays, column wise are taken and a row is created.
 #Note that the number of elements in the i/p arrays must be same.
+# The I/P arrays can be only of 1 dimentional
 print(np.column_stack((a1, a2)))
 print(np.column_stack((a1, a2, a3)))
