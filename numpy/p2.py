724f6cb5444480ce19b9df8d74c6ec5c83f6bea3
neelmyna <nithincheeta@gmail.com>
2025-05-15 10:38:10 +0530

    numpy programs added

--- numpy/p2.py
+++ numpy/p2.py
@@ -6,3 +6,10 @@ print(arr2)
 print(type(arr2))
 print(arr2[0], '    ', type(arr2[0]))
 print(arr2[0][0], '     ', type(arr2[0][0]))
+#---------------------
+
+arr1 = np.zeros((3, 5))
+print(arr1)
+
+print(arr1[2][0]) # 3rd row 1st element
+print(type(arr1[2][0])) #numpy.float64 is nothing but double in C/C++/Java
---
+++ numpy/p3.py
@@ -0,0 +1,10 @@
+#To initialize an Array/List of multi dimentional, with all elements Zero, we use the method zeroes()
+import numpy as np
+
+my_number = int(input('Enter a number of your choice: '))
+array2 = np.full((1, 5), my_number) # Creates numpy array (ndarray) with a List of numbers where the value of the number is specified by the user.
+print(array2)
+
+print(array2[0]) # Prints the 1st list in the 2D list
+print(array2[0][1]) # Prints the 2nd element in the 1st list
+print(array2[1][0]) # Error. There is no 2nd list
