e23c3ddc8faf90a836c961ca30a541110ba7bc20
neelmyna <nithincheeta@gmail.com>
2025-05-15 14:22:12 +0530

    p1 program in numpy updated

--- numpy/p1.py
+++ numpy/p1.py
@@ -1,8 +1,12 @@
 import numpy as np
 
-array = np.array([[1, 1, 3, 3, 4, 4, 4, 5, 7, 7, 8, 9, 12]])
-print(array)
-mean   = np.mean(array)
-median = np.median(array)
-print(f'Mean = {mean}')
-print(f'Median = {median}')
+array = np.array([[1, 1, 3, 3.3, 4.4, 4, 4, 5, 7, 7, 8, 9, 12]])
+print('Array: \n', array)
+print('Type of Array =', type(array))
+print('Array[0]: \n', array[0])
+print('Type of Array[0] =', type(array[0]))
+print('Array[0][0]: \n', array[0][0])
+print('Type of Array[0][0]: \n', type(array[0][0]))
+
+print('Array[1]: \n', array[1])
+print('Type of Array[1] =', type(array[1]))
